1dd7f27d743408dbb876a7b728b5a34f53f7c4cf
Marcial Lalanda <mlalandag@gmail.com>
2019-04-16 17:40:34 +0200

    Add files via upload

---
+++ task.py
@@ -0,0 +1,78 @@
+import numpy as np
+from physics_sim import PhysicsSim
+
+class Task():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5., target_pos=None):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+
+        # Goal
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+
+    def get_reward(self):
+        #"""Uses current pose of sim to return reward."""
+        #reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        reward = 0
+            
+        if self.sim.v[0] > 0 or self.sim.v[0] < 0:
+            reward -= 1
+
+        if self.sim.v[1] > 0 or self.sim.v[1] < 0:
+            reward -= 1
+
+        if self.sim.v[2] > 0:
+            reward += 5
+        
+        # Then the drone needs to keep being stable and avoid rotation. penalize every increase in euler angles.
+
+        euler_angles = np.clip(np.array([self.sim.pose[3:6].sum()]),-1,1)
+        reward -= euler_angles[0]
+        
+        #And to conclude the drone needs to reach the height goal of 150, reward him when he approach it and penalize him when he get far from it.
+
+        lateral_distance = np.clip(np.array([((self.sim.pose[0] - self.target_pos[0]) + (self.sim.pose[1] - self.target_pos[1])) ** 2]),-1,1)
+        reward -= lateral_distance[0] 
+         
+        vertical_distance = (self.sim.pose[2] - self.target_pos[2]) ** 2
+        
+        if vertical_distance != 0:
+            reward += 100 / vertical_distance
+            
+        return reward
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            reward += self.get_reward()
+            # penalize crash
+            if done and self.sim.time < self.sim.runtime:
+                reward = -10            
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
